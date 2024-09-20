e9ef15f89e16489b118ce1a54b3653a94b722100
Yujin Kim <yujii22001@gmail.com>
2024-09-20 20:24:04 +0900

    dispatch 버튼 테스트 & Context 생성

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,104 @@ import Detail from "./pages/Detail";
 import Edit from "./pages/Edit";
 import NotFound from "./pages/NotFound";
 import { Routes, Route } from "react-router-dom";
+import { useReducer, useRef, useState } from "react";
+import { createContext } from "react";
+
+const mockData = [
+  {
+    id: 1,
+    title: "1번 제목",
+    createdDate: new Date().getTime(),
+    newDate: new Date().getTime(),
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    title: "2번 제목",
+    createdDate: new Date().getTime(),
+    newDate: new Date().getTime(),
+    content: "2번 일기 내용",
+  },
+];
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) =>
+        String(action.data.id) === String(item.id) ? action.data : item
+      );
+    case "DELETE":
+      return state.filter((item) => String(action.id) !== String(item.id));
+    default:
+      return state;
+  }
+}
+
+export const StateContext = createContext();
+export const DispatchContext = createContext();
 
 function App() {
+  const [data, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (createdDate, content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        createdDate,
+        content,
+      },
+    });
+  };
+  const onUpdate = (id, newDate, content) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id,
+        newDate,
+        content,
+      },
+    });
+  };
+  const onDelete = (id) => {
+    dispatch({
+      type: "DELETE",
+
+      id,
+    });
+  };
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/new" element={<New />} />
-        <Route path="/detail" element={<Detail />} />
-        <Route path="/edit" element={<Edit />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <button onClick={() => onCreate(new Date().getTime(), "추가된 글 내용")}>
+        글 추가하기
+      </button>
+      <button
+        onClick={() => onUpdate(1, new Date().getTime(), "수정된 글 내용")}
+      >
+        글 수정하기
+      </button>
+      <button onClick={() => onDelete(1)}>글 삭제하기</button>
+
+      <StateContext.Provider value={data}>
+        <DispatchContext.Provider
+          value={{
+            onCreate,
+            onUpdate,
+            onDelete,
+          }}
+        >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/new" element={<New />} />
+            <Route path="/detail/:id" element={<Detail />} />
+            <Route path="/edit/:id" element={<Edit />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </DispatchContext.Provider>
+      </StateContext.Provider>
     </>
   );
 }
