f5c4467b06e5e89b4a4985a278c39bf1beaff434
Yujin Kim <yujii22001@gmail.com>
2024-09-20 23:43:36 +0900

    Feat: 기본 구성

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -2,14 +2,17 @@ import { useParams } from "react-router-dom";
 import Header from "../components/Header";
 import Editor from "../components/Editor";
 import ThreeButton from "../components/ThreeButton";
+import { useContext } from "react";
+import { StateContext } from "../App";
 
 const Detail = () => {
+  const data = useContext(StateContext);
   const params = useParams();
   return (
     <div>
       <Header title={"글 조회하기"} />
       <Editor />
-      <ThreeButton />
+      <ThreeButton data={data} />
     </div>
   );
 };
