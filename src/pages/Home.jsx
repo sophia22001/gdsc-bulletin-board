14e2b30f51cbffea5503c331e67621b5f46e986b
Yujin Kim <yujii22001@gmail.com>
2024-09-20 23:13:19 +0900

    Feat: data 전달

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,15 @@
+import Header from "../components/Header";
+import WriteList from "../components/WriteList";
+import { useContext } from "react";
+import { StateContext } from "../App";
+
 const Home = () => {
-  return <div>home</div>;
+  const data = useContext(StateContext);
+  return (
+    <div>
+      <Header title={"전체 글 보기"} />
+      <WriteList data={data} />
+    </div>
+  );
 };
 export default Home;
