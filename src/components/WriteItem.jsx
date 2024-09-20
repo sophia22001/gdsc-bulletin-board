0925713131c99a4d99aa5a7add00c11caf9ddc11
Yujin Kim <yujii22001@gmail.com>
2024-09-20 21:54:43 +0900

    Feat: data 적용 & 삭제 기능

---
+++ src/components/WriteItem.jsx
@@ -0,0 +1,32 @@
+import "./WriteItem.css";
+import Button from "./Button";
+import { useNavigate, useParams } from "react-router-dom";
+import { useContext } from "react";
+import { DispatchContext } from "../App";
+
+const WriteItem = ({ id, title, content, createdDate, newDate }) => {
+  const nav = useNavigate();
+  const { onDelete } = useContext(DispatchContext);
+
+  const onClickDelete = () => {
+    console.log(id);
+    if (window.confirm("정말 삭제하시겠습니까?")) {
+      onDelete(id);
+    }
+  };
+
+  return (
+    <div className="writeItem">
+      <div className="Info" onClick={() => nav(`/detail/${id}`)}>
+        <div className="title">{title}</div>
+        <div className="content">{content}</div>
+        <div className="date">생성: 2022.09.08 / 수정: 2024.10.10</div>
+      </div>
+
+      <div className="button">
+        <Button onClick={onClickDelete} text="삭제하기" type="NEGATIVE" />
+      </div>
+    </div>
+  );
+};
+export default WriteItem;
