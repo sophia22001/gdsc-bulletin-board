377bead0a6e838b90ee94b5b918a2e81224b97f3
Yujin Kim <yujii22001@gmail.com>
2024-09-22 16:30:55 +0900

    Fix: title은 input태그로

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,12 +1,45 @@
 import "./Editor.css";
 import ThreeButton from "./ThreeButton";
 import TwoButton from "./TwoButton";
+import { useState } from "react";
 
 const Editor = () => {
+  //입력 값을 저장하는 input state
+  const [input, setInput] = useState({
+    title: "",
+    createdDate: new Date().toLocaleDateString(),
+    content: "",
+  });
+
+  //이벤트 핸들러
+  const onChangeInput = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    console.log(name, value);
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
   return (
     <div className="editor">
-      <div className="title">제목</div>
-      <textarea className="content">내용</textarea>
+      <input
+        onChange={onChangeInput}
+        name="title"
+        value={input.title}
+        className="title"
+      />
+      <textarea
+        onChange={onChangeInput}
+        name="content"
+        value={input.content}
+        className="content"
+      />
+
+      <div className="created-date">
+        <p>생성 날짜: &nbsp;</p>
+        <div>{new Date().toLocaleDateString()}</div>
+      </div>
     </div>
   );
 };
